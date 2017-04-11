e9f70b8e8305dee363d20de6f12a08a6f298de92
JR Cologne <habichvergessen40@gmail.com>
2017-04-11 11:27:10 +0200

    Improve db class and error handling, Add usage example

--- DB.php
+++ DB.php
@@ -1,34 +1,76 @@
 <?php
   class DB {  
     public $pdo = null;
-    protected $errors = [ 'code' => 0, 'msg' => null ];
+    protected static $env = 'production';
+    protected static $error_types = [
+      0 => 'success',
+      1 => 'Connection to database failed',
+      2 => 'Selecting/Getting data from database failed',
+      3 => 'Inserting data into database failed',
+      4 => 'Deleting data from database failed',
+      5 => 'Updating data in database failed',
+    ];
+    protected $error = [ 'code' => 0, 'msg' => null ];
+
+    // initialize error handler / define configs for error handling
+    public static function initErrorHandler(array $error_types=[], string $env='production') {
+      self::$env = $env;
+
+      if ($error_types == []) {
+        self::$error_types = self::$error_types;
+      } else {
+        self::$error_types = $error_types;
+      }
+    }
+
+    // check for error
+    public function error() {
+      $error = $this->error;
+
+      return ( !empty($error) && $error['code'] != 0 && $error['msg'] != null ) ? true : false;
+    }
+
+    // get error
+    public function getError() {
+      $env = self::$env;
+      $error_types = self::$error_types;
+      $error = $this->error;
+
+      if (empty($error)) {
+        return null;
+      }
+
+      switch ($env) {
+        case 'production':
+          unset($error['pdo_exception']);
+          return $error;
+          break;
+
+        case 'development':
+        case 'dev':
+          return $error;
+          break;
+        
+        default:
+          return false;
+          break;
+      }
+    }
 
     // connect to database
     public function __construct(string $dbname, string $user, string $password, string $db_type='mysql', string $host='localhost', int $pdo_err_mode=PDO::ERRMODE_EXCEPTION) {
-      $errors = $this->errors;
+      $error_types = self::$error_types;
 
       try {
         $pdo = new PDO($db_type . ':host=' . $host . ';dbname=' . $dbname, $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, $pdo_err_mode);
       } catch (PDOException $e) {
-        $errors = [ 'code' => 1, 'msg' => 'Connection to database failed' ];
+        $this->error = [ 'code' => 1, 'msg' => $error_types[1], 'pdo_exception' => $e ];
         $pdo = null;
       }
 
-      $this->errors = $errors;
+      $this->error = $this->getError();
       $this->pdo = $pdo;
-    }
-
-    // check for errors
-    public function checkErrors() {
-      $errors = $this->errors;
-
-      return ( !empty($errors) && $errors['code'] != 0 && $errors['msg'] != null ) ? true : false;
-    }
-
-    // get all errors
-    public function getErrors() {
-      return $this->errors;
     }
 
     // check if connection to database is established
@@ -38,6 +80,7 @@
 
     // select/get data from database
     public function select(string $sql, $where=null, $fetch_mode=PDO::FETCH_ASSOC) {
+      $error_types = self::$error_types;
       $pdo = $this->pdo;
 
       try {
@@ -51,8 +94,8 @@
 
         $results = $statement->fetchAll($fetch_mode);
       } catch (PDOException $e) {
-        $errors = [ 'code' => 2, 'msg' => 'Selecting/getting data from database failed' ];
-        return $errors;
+        $this->error = [ 'code' => 2, 'msg' => $error_types[2], 'pdo_exception' => $e ];
+        return $this->getError();
       }
 
       if ($execution === true && !empty($results)) {
@@ -66,14 +109,15 @@
 
     // insert data into database
     public function insert(string $sql, array $values) {
+      $error_types = self::$error_types;
       $pdo = $this->pdo;
 
       try {
         $statement = $pdo->prepare($sql);
         $execution = $statement->execute($values);
       } catch (PDOException $e) {
-        $errors = [ 'code' => 3, 'msg' => 'Inserting data into database failed' ];
-        return $errors;
+        $this->error = [ 'code' => 3, 'msg' => $error_types[3], 'pdo_exception' => $e ];
+        return $this->getError();
       }
 
       if ($execution) {
@@ -85,6 +129,7 @@
 
     // delete data/rows from database
     public function delete(string $sql, $where=null) {
+      $error_types = self::$error_types;
       $pdo = $this->pdo;
 
       try {
@@ -96,8 +141,8 @@
           $execution = $statement->execute();
         }
       } catch (PDOException $e) {
-        $errors = [ 'code' => 4, 'msg' => 'Deleting data/rows from database failed' ];
-        return $errors;
+        $this->error = [ 'code' => 4, 'msg' => $error_types[4], 'pdo_exception' => $e ];
+        return $this->getError();
       }
 
       if ($execution) {
@@ -109,6 +154,7 @@
 
     // update data/rows in database
     public function update(string $sql, $values) {
+      $error_types = self::$error_types;
       $pdo = $this->pdo;
 
       try {
@@ -116,8 +162,8 @@
 
         $execution = $statement->execute($values);
       } catch (PDOException $e) {
-        $errors = [ 'code' => 5, 'msg' => 'Updating data/rows in database failed' ];
-        return $errors;
+        $this->error = [ 'code' => 5, 'msg' => $error_types[5], 'pdo_exception' => $e ];
+        return $this->getError();
       }
 
       if ($execution) {
