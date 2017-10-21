bfeb65b5ae3b058b7ffc28e174e85f984f890768
JR Cologne <kontakt@jr-cologne.de>
2017-10-21 22:00:52 +0200

    Update UnsupportedKeywordException.php

--- src/Exceptions/UnsupportedKeywordException.php
+++ src/Exceptions/UnsupportedKeywordException.php
@@ -1,9 +1,34 @@
 <?php
 
+/**
+ * A simple database class with PHP, PDO and a query builder.
+ *
+ * The class extends PDO for more control and in order to keep all features of PDO.
+ *
+ * PHP version >= 7.0
+ *
+ * LICENSE: MIT, see LICENSE file for more information
+ *
+ * @package jr-cologne/db-class
+ * @author JR Cologne <kontakt@jr-cologne.de>
+ * @copyright 2017 JR Cologne
+ * @license https://github.com/jr-cologne/db-class/blob/master/LICENSE MIT
+ * @version v2.0.1
+ * @link https://github.com/jr-cologne/db-class GitHub Repository
+ * @link https://packagist.org/packages/jr-cologne/db-class Packagist site
+ *
+ * ________________________________________________________________________________
+ *
+ * UnsupportedKeywordException.php
+ *
+ * An exception which is thrown when an unsupported keyword is used in the method DB::retrieve().
+ * 
+ */
+
 namespace JRCologne\Utils\Database\Exceptions;
 
 use \Exception;
 
-class UnsupportedKeywordExceptionException extends Exception {
+class UnsupportedKeywordException extends Exception {
   
 }
